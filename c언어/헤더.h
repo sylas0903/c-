5fa9f388d069a063568991826e465a08bd91e307
bluff <bluff@DESKTOP-USMT1DU>
2022-08-09 21:21:38 +0900

    초기 커밋.

---
+++ c언어/헤더.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdio.h>
+// 문자, 정수, 실수
+     // char (문자) <- 한 문자만 저장할 수 있는 자료형
+     // int (정수) <- 정수를 저장할 수 있는 자료형
+     // float (실수) <- 실수를 저장할 수 있는 자료형
+
+     // int [ (메모리 공간) box(100) ]
+
+int box = 100;
+printf("%\d", box);
